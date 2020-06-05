465dc6afbe39565a6aa514adac7648dbc4051722
krhskim07 <krhskim@gmail.com>
2020-06-05 18:15:00 +0200

    2nd commit

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
-<div class="container">
+<div class="container contact-area">
 <h1 class="text-center">Contact</h1>
 <hr>
   <div class="row">
